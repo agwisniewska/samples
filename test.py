e0e97a57234bdea25219d4a01786f7a0f8323b78
Agata Wiśniewska <agata.wisniewska.skygate@gmail.com>
2016-02-05 14:06:19 +0100

    some changes

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+import time, re
 
 
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
+
 driver.get("http://www.google.pl")
-assert Google in driver.title
+assert "Google" in driver.title
 pole = driver.find_element_by_id("lst-ib")
 pole.send_keys("python")
-pole.send_keys(Keys.Return)
-assert Python in driver.title
+button = driver.find_element_by_name("btnG")
+button.click()
+wait = WebDriverWait(driver, 3)
+wait.until(lambda d : d.title.lower().startswith('python'))
+assert "python" in driver.title
 driver.close()
